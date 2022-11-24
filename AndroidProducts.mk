e2ed2e84a509b7055e6fa3d33b4c183986ae99e1
NaassomDourado <naassomdourado@gmail.com>
2022-11-24 10:21:09 -0300

    Blissify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_pyxis.mk
+    $(LOCAL_DIR)/bliss_pyxis.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_pyxis-user \
-    lineage_pyxis-userdebug \
-    lineage_pyxis-eng
+    bliss_pyxis-user \
+    bliss_pyxis-userdebug \
+    bliss_pyxis-eng
